7d796424bb19ad8bdd86592fe00533f83a9dcbca
perrym-g <162512972+perrym-g@users.noreply.github.com>
2025-02-13 15:55:07 +1100

    functionality completed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 
+
 function Book(title, author, publishDate, read){
     this.title = title;
     this.author = author;
@@ -16,10 +17,12 @@ function addBookToLibrary(title, author, publishDate, read){
 function displayBooks(){
     // loop through array
     // for each book create a card on the page
+    reset();
     const display = document.getElementById("display");
-    myLibrary.forEach(book => {   
+    myLibrary.forEach((book, i) => {    
         const div = document.createElement("div");
         div.className = "card"
+        div.setAttribute("id", `${i}`);
 
         const title = document.createElement("h3");
         title.appendChild(document.createTextNode(`${book.title}`));
@@ -30,15 +33,42 @@ function displayBooks(){
         const publishDate = document.createElement("p");
         publishDate.appendChild(document.createTextNode(`${book.publishDate}`));
 
-        const read = document.createElement("p");
-        read.appendChild(document.createTextNode(`${book.read}`));
+        const read = document.createElement("input");
+        read.setAttribute("type", "checkbox");
+        read.setAttribute("name", "readbook");
 
+        const label = document.createElement("label");
+        label.htmlFor = "readbook";
+        label.innerHTML = "Read?";
+        read.checked = book.read;
+        console.log(book.read);
+
+        // console.log(i);
+
+
+        
+        const deleteBtn = document.createElement("button");
+        deleteBtn.className = "deleteBtn";
+        deleteBtn.innerHTML = "Delete";
+
+        read.addEventListener("change", () => {
+            book.readStatus();
+            console.log(book);
+        })
+
+        deleteBtn.addEventListener("click", () => {
+            let index = div.getAttribute("id");
+            console.log(index);
+            myLibrary.splice(i, 1);
+            displayBooks();
+        })
 
         div.appendChild(title);
         div.appendChild(author);
         div.appendChild(publishDate);
+        div.appendChild(label);
         div.appendChild(read);
-
+        div.appendChild(deleteBtn);
         display.appendChild(div);
     });
 }
@@ -47,10 +77,13 @@ function reset(){
     const display = document.getElementById("display");
     display.innerHTML = "";
 }
+
+Book.prototype.readStatus = function(){
+    this.read = !this.read;
+}
 const dialog = document.querySelector("dialog");
 const showBtn = document.querySelector("dialog + button");
 const closeBtn = document.querySelector("dialog button");
-
 showBtn.addEventListener("click", () => {
     dialog.showModal();
 })
@@ -66,7 +99,7 @@ addBookForm.addEventListener("submit", (e) =>{
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let publishDate = document.getElementById("publishDate").value;
-    let read = document.getElementById("read").value;
+    let read = document.getElementById("read").checked;
 
     addBookToLibrary(title, author, publishDate, read);
     dialog.close();
